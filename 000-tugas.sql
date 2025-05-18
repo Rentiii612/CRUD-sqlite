8ea0d43f189837ef89570dc8cb6677c1bf306b29
Rentiii612 <yorentianarafu@gmail.com>
2025-05-18 08:44:33 +0700

    menambahkan file database

---
+++ 000-tugas.sql
@@ -0,0 +1,17 @@
+DROP TABLE IF EXISTS tugas;
+
+CREATE TABLE "tugas" (
+  "id" INTEGER PRIMARY KEY AUTOINCREMENT,
+  "deskripsi" TEXT NOT NULL,
+  "waktu" INTEGER NOT NULL
+);
+
+INSERT INTO "tugas" ("deskripsi", "waktu") VALUES
+('makan pagi', 60),
+('kuliah', 360),
+('latihan bulu tangkis', 60),
+('olah raga', 150),
+('jalan-jalan', 100),
+('berenang', 60),
+('tidur', 480);
+
